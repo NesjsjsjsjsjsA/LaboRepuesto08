ae0886baa5dcf59623543ef55efa74b82a6cc33c
NesjsjsjsjsjsA <00133723@uca.edu.sv>
2024-05-29 03:57:40 -0600

    Interfaz semiCompleta

--- Labo08/src/main/java/fxvisual/labo08/HelloController.java
+++ Labo08/src/main/java/fxvisual/labo08/HelloController.java
@@ -11,9 +11,13 @@ public class HelloController implements Initializable
 {
 
     @FXML
+    private Spinner<Integer> spnVegetales;
+    @FXML
     private Spinner<Integer> spnCarne;
     @FXML
     private Spinner<Integer> spnPollo;
+    @FXML
+    private Spinner<Integer> spnPapa;
     @FXML
     private TextField txtfieldNombre;
     @FXML
@@ -28,25 +32,101 @@ public class HelloController implements Initializable
     private RadioButton rbtmEmpleado;
     @FXML
     private RadioButton rbtmEstudiante;
+    @FXML
+    private ToggleGroup Pagos;
+    @FXML
+    private ToggleGroup Cliente;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
         SpinnerValueFactory<Integer> valorCarne = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,10);
         SpinnerValueFactory<Integer> valorPollo = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,10);
+        SpinnerValueFactory<Integer> valorPapa = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,10);
+        SpinnerValueFactory<Integer> valorVegetales = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,10);
 
         valorCarne.setValue(0);
         valorPollo.setValue(0);
+        valorPapa.setValue(0);
+        valorVegetales.setValue(0);
 
         spnCarne.setValueFactory(valorCarne);
-
-
-        ToggleGroup tipoCliente = new ToggleGroup();
-
-        ToggleGroup tipoPago = new ToggleGroup();
+        spnPollo.setValueFactory(valorPollo);
+        spnPapa.setValueFactory(valorPapa);
+        spnVegetales.setValueFactory(valorVegetales);
 
     }
-    
 
+    public void AvisoMensajeComprar()
+    {
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
 
+        alerta.setTitle("Informacion");
+        alerta.setHeaderText(null);
+
+            alerta.setContentText(
+                    "Nombre del cliente : "+txtfieldNombre.getText() +
+                            "\nForma de pago: " + Buttom() +
+                            "\nTipo de cliente: "+Buttom2() +
+                            "\nDescuento: "+Descuento()
+            );
+
+            alerta.showAndWait();
+
+    }
+
+    public void LimpiarBarra()
+    {
+        txtfieldNombre.setText(" ");
+        rbtmEfectivo.setSelected(false);
+        rbtmEstudiante.setSelected(false);
+        rbtmEmpleado.setSelected(false);
+        rbtmTarjeta.setSelected(false);
+    }
+
+    public String Buttom()
+    {
+        String name = "none";
+
+        if (rbtmTarjeta.isSelected())
+        {
+            name = "Tarjeta";
+        }
+        if (rbtmEfectivo.isSelected())
+        {
+            name = "Efectivo";
+        }
+        return name;
+    }
+
+    public String Buttom2()
+    {
+        String name = "none";
+
+        if (rbtmEmpleado.isSelected())
+        {
+            name = "Empleado";
+        }
+        if (rbtmEstudiante.isSelected())
+        {
+            name = "Estudiante";
+        }
+        return name;
+    }
+    public double Descuento()
+    {
+        double desc = 0;
+
+        if (rbtmEmpleado.isSelected())
+        {
+            desc = 0.05;
+        }
+        if (rbtmEstudiante.isSelected())
+        {
+            desc = 0.10;
+        }
+
+        return desc;
+    }
 }
